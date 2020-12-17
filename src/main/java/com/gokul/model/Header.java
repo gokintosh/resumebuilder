4daf4d29780a5e8337491514bce0f2bdfad28631
GOKUL KANNAMBALLY BALACHNADRAN <52125327+gokintosh@users.noreply.github.com>
2020-12-17 19:20:37 +0100

    finished model

--- src/main/java/com/gokul/model/Header.java
+++ src/main/java/com/gokul/model/Header.java
@@ -1,4 +1,81 @@
 package com.gokul.model;
 
 public class Header {
+    private String name;
+    private String address;
+    private String emailAdress;
+    private String phoneNumber;
+    private String github;
+    private String linkedin;
+    private String website;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmailAdress() {
+        return emailAdress;
+    }
+
+    public void setEmailAdress(String emailAdress) {
+        this.emailAdress = emailAdress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getGithub() {
+        return github;
+    }
+
+    public void setGithub(String github) {
+        this.github = github;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    @Override
+    public String toString() {
+        return "Header{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", emailAdress='" + emailAdress + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", github='" + github + '\'' +
+                ", linkedin='" + linkedin + '\'' +
+                ", website='" + website + '\'' +
+                '}';
+    }
 }
